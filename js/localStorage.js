90d19f5a1f6104333dfd0161c6144f810bbad914
Keanottesen <47158972+Keanottesen@users.noreply.github.com>
2019-11-04 09:41:48 +0100

    small changes

---
+++ js/localStorage.js
@@ -0,0 +1,539 @@
+//hardcoded albums
+let albums = [
+	{
+    'id': 1,
+    'title': 'Bacon and Eggs',
+    'artistId': 2,
+    'genreId': 4,
+    'pathToPicture': 'assets/images/artwork/clearday.jpg'
+  },
+  {
+    'id': 2,
+    'title': 'Pizza head',
+    'artistId': 5,
+    'genreId': 10,
+    'pathToPicture': 'assets/images/artwork/energy.jpg'
+  },
+  {
+    'id': '3',
+    'title': 'Summer Hits',
+    'artistId': 3,
+    'genreId': 1,
+    'pathToPicture': 'assets/images/artwork/goinghigher.jpg'
+  },
+  {
+    'id': 4,
+    'title': 'The movie soundtrack',
+    'artistId': 2,
+    'genreId': 9,
+    'pathToPicture': 'assets/images/artwork/funkyelement.jpg'
+  },
+  {
+    'id': 5,
+    'title': 'Best of the Worst',
+    'artistId': 1,
+    'genreId': 3,
+    'pathToPicture': 'assets/images/artwork/popdance.jpg'
+  },
+  {
+    'id': 6,
+    'title': 'Hello World',
+    'artistId': 3,
+    'genreId': 6,
+    'pathToPicture': 'assets/images/artwork/ukulele.jpg'
+  },
+  {
+    'id': 7,
+    'title': 'Best beats',
+    'artistId': 4,
+    'genreId': 7,
+    'pathToPicture': 'assets/images/artwork/sweet.jpg'
+  }
+];
+pushToLocalStorage(albums, 'albums')
+let artist = [
+	{
+    'id': 1,
+    'name': 'Mickey Mouse'
+  },
+  {
+    'id': 2,
+    'name': 'Goofy'
+  },
+  {
+    'id': 3,
+    'name': 'Bart Simpson'
+  },
+  {
+    'id': 4,
+    'name': 'Homer'
+  },
+  {
+    'id': 5,
+    'name': 'Bruce Lee'
+  }
+];
+pushToLocalStorage(artist, 'artist')
+let songs = [
+	{
+    'id': 1,
+    'title': 'Acoustic Breeze',
+    'artistId': 1,
+    'albumId': 5,
+    'genreId': 8,
+    'duration': '2:37',
+    'path': 'assets/music/bensound-acousticbreeze.mp3',
+    'albumOrder': 1,
+    'plays': 10
+  },
+  {
+    'id': 2,
+    'title': 'A new beginning',
+    'artistId': 1,
+    'albumId': 5,
+    'genreId': 1,
+    'duration': '2:35',
+    'path': 'assets/music/bensound-anewbeginning.mp3',
+    'albumOrder': 2,
+    'plays': 4
+  },
+  {
+    'id': 3,
+    'title': 'Better Days',
+    'artistId': 1,
+    'albumId': 5,
+    'genreId': 2,
+    'duration': '2:33',
+    'path': 'assets/music/bensound-betterdays.mp3',
+    'albumOrder': 3,
+    'plays': 10
+  },
+  {
+    'id': 4,
+    'title': 'Buddy',
+    'artistId': 1,
+    'albumId': 5,
+    'genreId': 3,
+    'duration': '2:02',
+    'path': 'assets/music/bensound-buddy.mp3',
+    'albumOrder': 4,
+    'plays': 13
+  },
+  {
+    'id': 5,
+    'title': 'Clear Day',
+    'artistId': 1,
+    'albumId': 5,
+    'genreId': 4,
+    'duration': '1:29',
+    'path': 'assets/music/bensound-clearday.mp3',
+    'albumOrder': 5,
+    'plays': 8
+  },
+  {
+    'id': 6,
+    'title': 'Going Higher',
+    'artistId': 2,
+    'albumId': 1,
+    'genreId': 1,
+    'duration': '4:04',
+    'path': 'assets/music/bensound-goinghigher.mp3',
+    'albumOrder': 1,
+    'plays': 34
+  },
+  {
+    'id': 7,
+    'title': 'Funny Song',
+    'artistId': 2,
+    'albumId': 4,
+    'genreId': 2,
+    'duration': '3:07',
+    'path': 'assets/music/bensound-funnysong.mp3',
+    'albumOrder': 2,
+    'plays': 12
+  },
+  {
+    'id': 8,
+    'title': 'Funky Element',
+    'artistId': 2,
+    'albumId': 1,
+    'genreId': 3,
+    'duration': '3:08',
+    'path': 'assets/music/bensound-funkyelement.mp3',
+    'albumOrder': 2,
+    'plays': 26
+  },
+  {
+    'id': 9,
+    'title': 'Extreme Action',
+    'artistId': 2,
+    'albumId': 1,
+    'genreId': 4,
+    'duration': '8:03',
+    'path': 'assets/music/bensound-extremeaction.mp3',
+    'albumOrder': 3,
+    'plays': 29
+  },
+  {
+    'id': 10,
+    'title': 'Epic',
+    'artistId': 2,
+    'albumId': 4,
+    'genreId': 5,
+    'duration': '2:58',
+    'path': 'assets/music/bensound-epic.mp3',
+    'albumOrder': 3,
+    'plays': 17
+  },
+  {
+    'id': 11,
+    'title': 'Energy',
+    'artistId': 2,
+    'albumId': 1,
+    'genreId': 6,
+    'duration': '2:59',
+    'path': 'assets/music/bensound-energy.mp3',
+    'albumOrder': 4,
+    'plays': 26
+  },
+  {
+    'id': 12,
+    'title': 'Dubstep',
+    'artistId': 2,
+    'albumId': 1,
+    'genreId': 7,
+    'duration': '2:03',
+    'path': 'assets/music/bensound-dubstep.mp3',
+    'albumOrder': 5,
+    'plays': 22
+  },
+  {
+    'id': 13,
+    'title': 'Happiness',
+    'artistId': 3,
+    'albumId': 6,
+    'genreId': 8,
+    'duration': '4:21',
+    'path': 'assets/music/bensound-happiness.mp3',
+    'albumOrder': 5,
+    'plays': 3
+  },
+  {
+    'id': 14,
+    'title': 'Happy Rock',
+    'artistId': 3,
+    'albumId': 6,
+    'genreId': 9,
+    'duration': '1:45',
+    'path': 'assets/music/bensound-happyrock.mp3',
+    'albumOrder': 4,
+    'plays': 8
+  },
+  {
+    'id': 15,
+    'title': 'Jazzy Frenchy',
+    'artistId': 3,
+    'albumId': 6,
+    'genreId': 10,
+    'duration': '1:44',
+    'path': 'assets/music/bensound-jazzyfrenchy.mp3',
+    'albumOrder': 3,
+    'plays': 11
+  },
+  {
+    'id': 16,
+    'title': 'Little Idea',
+    'artistId': 3,
+    'albumId': 6,
+    'genreId': 1,
+    'duration': '2:49',
+    'path': 'assets/music/bensound-littleidea.mp3',
+    'albumOrder': 2,
+    'plays': 12
+  },
+  {
+    'id': 17,
+    'title': 'Memories',
+    'artistId': 3,
+    'albumId': 6,
+    'genreId': 2,
+    'duration': '3:50',
+    'path': 'assets/music/bensound-memories.mp3',
+    'albumOrder': 1,
+    'plays': 6
+  },
+  {
+    'id': 18,
+    'title': 'Moose',
+    'artistId': 4,
+    'albumId': 7,
+    'genreId': 1,
+    'duration': '2:43',
+    'path': 'assets/music/bensound-moose.mp3',
+    'albumOrder': 5,
+    'plays': 2
+  },
+  {
+    'id': 19,
+    'title': 'November',
+    'artistId': 4,
+    'albumId': 7,
+    'genreId': 2,
+    'duration': '3:32',
+    'path': 'assets/music/bensound-november.mp3',
+    'albumOrder': 4,
+    'plays': 5
+  },
+  {
+    'id': 20,
+    'title': 'Of Elias Dream',
+    'artistId': 4,
+    'albumId': 7,
+    'genreId': 3,
+    'duration': '4:58',
+    'path': 'assets/music/bensound-ofeliasdream.mp3',
+    'albumOrder': 3,
+    'plays': 5
+  },
+  {
+    'id': 21,
+    'title': 'Pop Dance',
+    'artistId': 4,
+    'albumId': 7,
+    'genreId': 2,
+    'duration': '2:42',
+    'path': 'assets/music/bensound-popdance.mp3',
+    'albumOrder': 2,
+    'plays': 11
+  },
+  {
+    'id': 22,
+    'title': 'Retro Soul',
+    'artistId': 4,
+    'albumId': 7,
+    'genreId': 5,
+    'duration': '3:36',
+    'path': 'assets/music/bensound-retrosoul.mp3',
+    'albumOrder': 1,
+    'plays': 11
+  },
+  {
+    'id': 23,
+    'title': 'Sad Day',
+    'artistId': 5,
+    'albumId': 2,
+    'genreId': 1,
+    'duration': '2:28',
+    'path': 'assets/music/bensound-sadday.mp3',
+    'albumOrder': 1,
+    'plays': 9
+  },
+  {
+    'id': 24,
+    'title': 'Sci-fi',
+    'artistId': 5,
+    'albumId': 2,
+    'genreId': 2,
+    'duration': '4:44',
+    'path': 'assets/music/bensound-scifi.mp3',
+    'albumOrder': 2,
+    'plays': 9
+  },
+  {
+    'id': 25,
+    'title': 'Slow Motion',
+    'artistId': 5,
+    'albumId': 2,
+    'genreId': 3,
+    'duration': '3:26',
+    'path': 'assets/music/bensound-slowmotion.mp3',
+    'albumOrder': 3,
+    'plays': 4
+  },
+  {
+    'id': 26,
+    'title': 'Sunny',
+    'artistId': 5,
+    'albumId': 2,
+    'genreId': 4,
+    'duration': '2:20',
+    'path': 'assets/music/bensound-sunny.mp3',
+    'albumOrder': 4,
+    'plays': 19
+  },
+  {
+    'id': 27,
+    'title': 'Sweet',
+    'artistId': 5,
+    'albumId': 2,
+    'genreId': 5,
+    'duration': '5:07',
+    'path': 'assets/music/bensound-sweet.mp3',
+    'albumOrder': 5,
+    'plays': 17
+  },
+  {
+    'id': 28,
+    'title': 'Tenderness ',
+    'artistId': 3,
+    'albumId': 3,
+    'genreId': 7,
+    'duration': '2:03',
+    'path': 'assets/music/bensound-tenderness.mp3',
+    'albumOrder': 4,
+    'plays': 13
+  },
+  {
+    'id': 29,
+    'title': 'The Lounge',
+    'artistId': 3,
+    'albumId': 3,
+    'genreId': 8,
+    'duration': '4:16',
+    'path': 'assets/music/bensound-thelounge.mp3 ',
+    'albumOrder': 3,
+    'plays': 7
+  },
+  {
+    'id': 30,
+    'title': 'Ukulele',
+    'artistId': 3,
+    'albumId': 3,
+    'genreId': 9,
+    'duration': '2:26',
+    'path': 'assets/music/bensound-ukulele.mp3 ',
+    'albumOrder': 2,
+    'plays': 22
+  },
+  {
+    'id': 31,
+    'title': 'Tomorrow',
+    'artistId': 3,
+    'albumId': 3,
+    'genreId': 1,
+    'duration': '4:54',
+    'path': 'assets/music/bensound-tomorrow.mp3 ',
+    'albumOrder': 1,
+    'plays': 15
+  }
+];
+pushToLocalStorage(songs, 'songs')
+let playlists = [
+	{
+    'id': 1,
+    'name': 'Playlist2',
+    'owner': 'reece-kenney',
+    'dateCreated': '2017-08-27 00:00:00'
+  },
+  {
+    'id': 2,
+    'name': 'Running Songs',
+    'owner': 'reece-kenney',
+    'dateCreated': '2017-08-27 00:00:00'
+  },
+  {
+    'id': 3,
+    'name': 'Classics',
+    'owner': 'reece-kenney',
+    'dateCreated': '2017-08-27 00:00:00'
+  },
+  {
+    'id': 4,
+    'name': 'Party',
+    'owner': 'reece-kenney',
+    'dateCreated': '2017-08-27 00:00:00'
+  },
+  {
+    'id': 5,
+    'name': 'This is a test',
+    'owner': 'reece-kenney',
+    'dateCreated': '2017-12-04 00:00:00'
+  },
+  {
+    'id': 6,
+    'name': 'Bulldozer',
+    'owner': 'reece-kenney',
+    'dateCreated': '2017-12-04 00:00:00'
+  },
+];
+pushToLocalStorage(playlists, 'playlists')
+let playlistSongs = [
+	{
+    'id': 6,
+    'songId': 17,
+    'playlistId': 2,
+    'playlistOrder': 4
+  },
+  {
+    'id': 8,
+    'songId': 16,
+    'playlistId': 5,
+    'playlistOrder': 0
+  },
+  {
+    'id': 9,
+    'songId': 15,
+    'playlistId': 3,
+    'playlistOrder': 0
+  },
+  {
+    'id': 10,
+    'songId': 14,
+    'playlistId': 4,
+    'playlistOrder': 0
+  },
+  {
+    'id': 11,
+    'songId': 17,
+    'playlistId': 3,
+    'playlistOrder': 1
+  },
+  {
+    'id': 12,
+    'songId': 16,
+    'playlistId': 3,
+    'playlistOrder': 2
+  },
+  {
+    'id': 13,
+    'songId': 16,
+    'playlistId': 5,
+    'playlistOrder': 1
+  },
+  {
+    'id': 14,
+    'songId': 14,
+    'playlistId': 3,
+    'playlistOrder': 3
+  },
+  {
+    'id': 15,
+    'songId': 5,
+    'playlistId': 5,
+    'playlistOrder': 2
+  },
+  {
+    'id': 16,
+    'songId': 23,
+    'playlistId': 4,
+    'playlistOrder': 1
+  },
+  {
+    'id': 17,
+    'songId': 6,
+    'playlistId': 2,
+    'playlistOrder': 5
+  },
+  {
+    'id': 18,
+    'songId': 29,
+    'playlistId': 3,
+    'playlistOrder': 4
+  },
+];
+pushToLocalStorage(playlistSongs, 'playlistSongs')
+
+function pushToLocalStorage(object, name) {
+  let storageObject = JSON.stringify(object);
+  localStorage.setItem(name, storageObject);
+}
--- js/script.js
+++ js/script.js
@@ -8,543 +8,14 @@ var repeat = false;
 var shuffle = false;
 var userLoggedIn;
 var timer;
-
-//hardcoded albums
-const albums =
-[
-	{
-    'id': 1,
-    'title': 'Bacon and Eggs',
-    'artistId': 2,
-    'genreId': 4,
-    'pathToPicture': 'assets/images/artwork/clearday.jpg'
-  },
-  {
-    'id': 2,
-    'title': 'Pizza head',
-    'artistId': 5,
-    'genreId': 10,
-    'pathToPicture': 'assets/images/artwork/energy.jpg'
-  },
-  {
-    'id': '3',
-    'title': 'Summer Hits',
-    'artistId': 3,
-    'genreId': 1,
-    'pathToPicture': 'assets/images/artwork/goinghigher.jpg'
-  },
-  {
-    'id': 4,
-    'title': 'The movie soundtrack',
-    'artistId': 2,
-    'genreId': 9,
-    'pathToPicture': 'assets/images/artwork/funkyelement.jpg'
-  },
-  {
-    'id': 5,
-    'title': 'Best of the Worst',
-    'artistId': 1,
-    'genreId': 3,
-    'pathToPicture': 'assets/images/artwork/popdance.jpg'
-  },
-  {
-    'id': 6,
-    'title': 'Hello World',
-    'artistId': 3,
-    'genreId': 6,
-    'pathToPicture': 'assets/images/artwork/ukulele.jpg'
-  },
-  {
-    'id': 7,
-    'title': 'Best beats',
-    'artistId': 4,
-    'genreId': 7,
-    'pathToPicture': 'assets/images/artwork/sweet.jpg'
-  }
-];
-
-const artist = [
-	{
-    'id': 1,
-    'name': 'Mickey Mouse'
-  },
-  {
-    'id': 2,
-    'name': 'Goofy'
-  },
-  {
-    'id': 3,
-    'name': 'Bart Simpson'
-  },
-  {
-    'id': 4,
-    'name': 'Homer'
-  },
-  {
-    'id': 5,
-    'name': 'Bruce Lee'
-  }
-];
-
-const songs = [
-	{
-    'id': 1,
-    'title': 'Acoustic Breeze',
-    'artistId': 1,
-    'albumId': 5,
-    'genreId': 8,
-    'duration': '2:37',
-    'path': 'assets/music/bensound-acousticbreeze.mp3',
-    'albumOrder': 1,
-    'plays': 10
-  },
-  {
-    'id': 2,
-    'title': 'A new beginning',
-    'artistId': 1,
-    'albumId': 5,
-    'genreId': 1,
-    'duration': '2:35',
-    'path': 'assets/music/bensound-anewbeginning.mp3',
-    'albumOrder': 2,
-    'plays': 4
-  },
-  {
-    'id': 3,
-    'title': 'Better Days',
-    'artistId': 1,
-    'albumId': 5,
-    'genreId': 2,
-    'duration': '2:33',
-    'path': 'assets/music/bensound-betterdays.mp3',
-    'albumOrder': 3,
-    'plays': 10
-  },
-  {
-    'id': 4,
-    'title': 'Buddy',
-    'artistId': 1,
-    'albumId': 5,
-    'genreId': 3,
-    'duration': '2:02',
-    'path': 'assets/music/bensound-buddy.mp3',
-    'albumOrder': 4,
-    'plays': 13
-  },
-  {
-    'id': 5,
-    'title': 'Clear Day',
-    'artistId': 1,
-    'albumId': 5,
-    'genreId': 4,
-    'duration': '1:29',
-    'path': 'assets/music/bensound-clearday.mp3',
-    'albumOrder': 5,
-    'plays': 8
-  },
-  {
-    'id': 6,
-    'title': 'Going Higher',
-    'artistId': 2,
-    'albumId': 1,
-    'genreId': 1,
-    'duration': '4:04',
-    'path': 'assets/music/bensound-goinghigher.mp3',
-    'albumOrder': 1,
-    'plays': 34
-  },
-  {
-    'id': 7,
-    'title': 'Funny Song',
-    'artistId': 2,
-    'albumId': 4,
-    'genreId': 2,
-    'duration': '3:07',
-    'path': 'assets/music/bensound-funnysong.mp3',
-    'albumOrder': 2,
-    'plays': 12
-  },
-  {
-    'id': 8,
-    'title': 'Funky Element',
-    'artistId': 2,
-    'albumId': 1,
-    'genreId': 3,
-    'duration': '3:08',
-    'path': 'assets/music/bensound-funkyelement.mp3',
-    'albumOrder': 2,
-    'plays': 26
-  },
-  {
-    'id': 9,
-    'title': 'Extreme Action',
-    'artistId': 2,
-    'albumId': 1,
-    'genreId': 4,
-    'duration': '8:03',
-    'path': 'assets/music/bensound-extremeaction.mp3',
-    'albumOrder': 3,
-    'plays': 29
-  },
-  {
-    'id': 10,
-    'title': 'Epic',
-    'artistId': 2,
-    'albumId': 4,
-    'genreId': 5,
-    'duration': '2:58',
-    'path': 'assets/music/bensound-epic.mp3',
-    'albumOrder': 3,
-    'plays': 17
-  },
-  {
-    'id': 11,
-    'title': 'Energy',
-    'artistId': 2,
-    'albumId': 1,
-    'genreId': 6,
-    'duration': '2:59',
-    'path': 'assets/music/bensound-energy.mp3',
-    'albumOrder': 4,
-    'plays': 26
-  },
-  {
-    'id': 12,
-    'title': 'Dubstep',
-    'artistId': 2,
-    'albumId': 1,
-    'genreId': 7,
-    'duration': '2:03',
-    'path': 'assets/music/bensound-dubstep.mp3',
-    'albumOrder': 5,
-    'plays': 22
-  },
-  {
-    'id': 13,
-    'title': 'Happiness',
-    'artistId': 3,
-    'albumId': 6,
-    'genreId': 8,
-    'duration': '4:21',
-    'path': 'assets/music/bensound-happiness.mp3',
-    'albumOrder': 5,
-    'plays': 3
-  },
-  {
-    'id': 14,
-    'title': 'Happy Rock',
-    'artistId': 3,
-    'albumId': 6,
-    'genreId': 9,
-    'duration': '1:45',
-    'path': 'assets/music/bensound-happyrock.mp3',
-    'albumOrder': 4,
-    'plays': 8
-  },
-  {
-    'id': 15,
-    'title': 'Jazzy Frenchy',
-    'artistId': 3,
-    'albumId': 6,
-    'genreId': 10,
-    'duration': '1:44',
-    'path': 'assets/music/bensound-jazzyfrenchy.mp3',
-    'albumOrder': 3,
-    'plays': 11
-  },
-  {
-    'id': 16,
-    'title': 'Little Idea',
-    'artistId': 3,
-    'albumId': 6,
-    'genreId': 1,
-    'duration': '2:49',
-    'path': 'assets/music/bensound-littleidea.mp3',
-    'albumOrder': 2,
-    'plays': 12
-  },
-  {
-    'id': 17,
-    'title': 'Memories',
-    'artistId': 3,
-    'albumId': 6,
-    'genreId': 2,
-    'duration': '3:50',
-    'path': 'assets/music/bensound-memories.mp3',
-    'albumOrder': 1,
-    'plays': 6
-  },
-  {
-    'id': 18,
-    'title': 'Moose',
-    'artistId': 4,
-    'albumId': 7,
-    'genreId': 1,
-    'duration': '2:43',
-    'path': 'assets/music/bensound-moose.mp3',
-    'albumOrder': 5,
-    'plays': 2
-  },
-  {
-    'id': 19,
-    'title': 'November',
-    'artistId': 4,
-    'albumId': 7,
-    'genreId': 2,
-    'duration': '3:32',
-    'path': 'assets/music/bensound-november.mp3',
-    'albumOrder': 4,
-    'plays': 5
-  },
-  {
-    'id': 20,
-    'title': 'Of Elias Dream',
-    'artistId': 4,
-    'albumId': 7,
-    'genreId': 3,
-    'duration': '4:58',
-    'path': 'assets/music/bensound-ofeliasdream.mp3',
-    'albumOrder': 3,
-    'plays': 5
-  },
-  {
-    'id': 21,
-    'title': 'Pop Dance',
-    'artistId': 4,
-    'albumId': 7,
-    'genreId': 2,
-    'duration': '2:42',
-    'path': 'assets/music/bensound-popdance.mp3',
-    'albumOrder': 2,
-    'plays': 11
-  },
-  {
-    'id': 22,
-    'title': 'Retro Soul',
-    'artistId': 4,
-    'albumId': 7,
-    'genreId': 5,
-    'duration': '3:36',
-    'path': 'assets/music/bensound-retrosoul.mp3',
-    'albumOrder': 1,
-    'plays': 11
-  },
-  {
-    'id': 23,
-    'title': 'Sad Day',
-    'artistId': 5,
-    'albumId': 2,
-    'genreId': 1,
-    'duration': '2:28',
-    'path': 'assets/music/bensound-sadday.mp3',
-    'albumOrder': 1,
-    'plays': 9
-  },
-  {
-    'id': 24,
-    'title': 'Sci-fi',
-    'artistId': 5,
-    'albumId': 2,
-    'genreId': 2,
-    'duration': '4:44',
-    'path': 'assets/music/bensound-scifi.mp3',
-    'albumOrder': 2,
-    'plays': 9
-  },
-  {
-    'id': 25,
-    'title': 'Slow Motion',
-    'artistId': 5,
-    'albumId': 2,
-    'genreId': 3,
-    'duration': '3:26',
-    'path': 'assets/music/bensound-slowmotion.mp3',
-    'albumOrder': 3,
-    'plays': 4
-  },
-  {
-    'id': 26,
-    'title': 'Sunny',
-    'artistId': 5,
-    'albumId': 2,
-    'genreId': 4,
-    'duration': '2:20',
-    'path': 'assets/music/bensound-sunny.mp3',
-    'albumOrder': 4,
-    'plays': 19
-  },
-  {
-    'id': 27,
-    'title': 'Sweet',
-    'artistId': 5,
-    'albumId': 2,
-    'genreId': 5,
-    'duration': '5:07',
-    'path': 'assets/music/bensound-sweet.mp3',
-    'albumOrder': 5,
-    'plays': 17
-  },
-  {
-    'id': 28,
-    'title': 'Tenderness ',
-    'artistId': 3,
-    'albumId': 3,
-    'genreId': 7,
-    'duration': '2:03',
-    'path': 'assets/music/bensound-tenderness.mp3',
-    'albumOrder': 4,
-    'plays': 13
-  },
-  {
-    'id': 29,
-    'title': 'The Lounge',
-    'artistId': 3,
-    'albumId': 3,
-    'genreId': 8,
-    'duration': '4:16',
-    'path': 'assets/music/bensound-thelounge.mp3 ',
-    'albumOrder': 3,
-    'plays': 7
-  },
-  {
-    'id': 30,
-    'title': 'Ukulele',
-    'artistId': 3,
-    'albumId': 3,
-    'genreId': 9,
-    'duration': '2:26',
-    'path': 'assets/music/bensound-ukulele.mp3 ',
-    'albumOrder': 2,
-    'plays': 22
-  },
-  {
-    'id': 31,
-    'title': 'Tomorrow',
-    'artistId': 3,
-    'albumId': 3,
-    'genreId': 1,
-    'duration': '4:54',
-    'path': 'assets/music/bensound-tomorrow.mp3 ',
-    'albumOrder': 1,
-    'plays': 15
-  }
-];
-
-const playlists = [
-	{
-    'id': 2,
-    'name': 'Playlist2',
-    'owner': 'reece-kenney',
-    'dateCreated': '2017-08-27 00:00:00'
-  },
-  {
-    'id': 3,
-    'name': 'Running Songs',
-    'owner': 'reece-kenney',
-    'dateCreated': '2017-08-27 00:00:00'
-  },
-  {
-    'id': 4,
-    'name': 'Classics',
-    'owner': 'reece-kenney',
-    'dateCreated': '2017-08-27 00:00:00'
-  },
-  {
-    'id': 5,
-    'name': 'Party',
-    'owner': 'reece-kenney',
-    'dateCreated': '2017-08-27 00:00:00'
-  },
-  {
-    'id': 6,
-    'name': 'This is a test',
-    'owner': 'reece-kenney',
-    'dateCreated': '2017-12-04 00:00:00'
-  },
-  {
-    'id': 7,
-    'name': 'Bulldozer',
-    'owner': 'reece-kenney',
-    'dateCreated': '2017-12-04 00:00:00'
-  },
-];
-
-const playlistSongs = [
-	{
-    'id': 6,
-    'songId': 17,
-    'playlistId': 2,
-    'playlistOrder': 4
-  },
-  {
-    'id': 8,
-    'songId': 16,
-    'playlistId': 5,
-    'playlistOrder': 0
-  },
-  {
-    'id': 9,
-    'songId': 15,
-    'playlistId': 3,
-    'playlistOrder': 0
-  },
-  {
-    'id': 10,
-    'songId': 14,
-    'playlistId': 4,
-    'playlistOrder': 0
-  },
-  {
-    'id': 11,
-    'songId': 17,
-    'playlistId': 3,
-    'playlistOrder': 1
-  },
-  {
-    'id': 12,
-    'songId': 16,
-    'playlistId': 3,
-    'playlistOrder': 2
-  },
-  {
-    'id': 13,
-    'songId': 16,
-    'playlistId': 5,
-    'playlistOrder': 1
-  },
-  {
-    'id': 14,
-    'songId': 14,
-    'playlistId': 3,
-    'playlistOrder': 3
-  },
-  {
-    'id': 15,
-    'songId': 5,
-    'playlistId': 5,
-    'playlistOrder': 2
-  },
-  {
-    'id': 16,
-    'songId': 23,
-    'playlistId': 4,
-    'playlistOrder': 1
-  },
-  {
-    'id': 17,
-    'songId': 6,
-    'playlistId': 2,
-    'playlistOrder': 5
-  },
-  {
-    'id': 18,
-    'songId': 29,
-    'playlistId': 3,
-    'playlistOrder': 4
-  },
-];
-
 let state = null;
+let playlists = JSON.parse(localStorage.playlists);
+let albums = JSON.parse(localStorage.albums);
+let artist = JSON.parse(localStorage.artist);
+let songs = JSON.parse(localStorage.songs);
+let users = JSON.parse(localStorage.users);
+let playlistSongs = JSON.parse(localStorage.playlistSongs);
+
 
 if (localStorage.getItem("state") === null) {
   state = {
@@ -577,6 +48,11 @@ const onAlbumClickHandler = (clicked) => {
 
   setState('album_id', clicked.id);
   window.location = 'album.html';
+}
+
+function pushToLocalStorage(object, name) {
+  const storageObject = JSON.stringify(object);
+  localStorage.setItem(name, storageObject);
 }
 
 
@@ -694,48 +170,30 @@ function createPlaylist() {
 
   var popup = prompt("Please enter the name of your playlist");
 
+	let users = JSON.parse(localStorage.users)
+	const userLoggedIn = users.find(x => x.active === true);
+
   if (popup != null) {
+		playlists.push({
+			    'id': JSON.parse(localStorage.playlists).length + 1,
+			    'name': popup,
+			    'owner': userLoggedIn.userName,
+			    'dateCreated': new Date()
+			  })
 
-    $.post("includes/handlers/ajax/createPlaylist.php", {
-        name: popup,
-        username: userLoggedIn
-      })
-      .done(function(error) {
+				this.pushToLocalStorage(playlists, 'playlists');
 
-        if (error != "") {
-          alert(error);
-          return;
-        }
-
-        //do something when ajax returns
-        openPage("yourMusic.php");
-      });
+        location.reload();
+      };
 
   }
-
-}
 
 function deletePlaylist(playlistId) {
-  var prompt = confirm("Are you sure you want to delte this playlist?");
-
-  if (prompt == true) {
-
-    $.post("includes/handlers/ajax/deletePlaylist.php", {
-        playlistId: playlistId
-      })
-      .done(function(error) {
-
-        if (error != "") {
-          alert(error);
-          return;
-        }
-
-        //do something when ajax returns
-        openPage("yourMusic.php");
-      });
 
 
-  }
+
+        window.location = 'yourMusic.html'
+
 }
 
 function hideOptionsMenu() {
@@ -881,6 +339,7 @@ $(document).ready(function() {
       break;
     case '/browse':
       //render data in html
+      console.log(albums);
       document.querySelector('.gridViewContainer').innerHTML = albums.map(album =>
         `<div class='gridViewItem'>
 			 <span id=${album.id} role='link' tabindex='0' onclick='onAlbumClickHandler(this)'>
@@ -893,8 +352,7 @@ $(document).ready(function() {
       ).join('')
       break;
     case '/yourMusic':
-      console.log(playlists);
-      document.querySelector('.playlistsGridContainer').innerHTML = playlists.map(playlist =>
+      document.querySelector('.playlistsGridContainer').innerHTML = JSON.parse(localStorage.playlists).map(playlist =>
         `      <div id='${playlist.id}'class='gridViewItem' role='link' tabindex='0'
 										onclick='playlistOnclickHandler(this)'>
 									<div class='playlistImage'>
@@ -944,12 +402,8 @@ $(document).ready(function() {
 
       break;
 		case '/settings':
-
-					let users = JSON.parse(localStorage.users)
-					console.log(users);
-					const userLoggedIn = users.find(x => x.active === true);
-					document.getElementById('activeUserName').innerHTML = userLoggedIn.firstName + ' ' + userLoggedIn.lastName;
-					console.log(userLoggedIn);
+		const userLoggedIn = users.find(x => x.active === true);
+		document.getElementById('activeUserName').innerHTML = userLoggedIn.firstName + ' ' + userLoggedIn.lastName;
 			break;
   }
 })
